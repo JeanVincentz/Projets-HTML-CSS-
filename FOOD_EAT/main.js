73ddc713473227c7b7ef0ab28bc9ece2340710f8
Jean Vincentz <jean.vincentz.dev@outlook.fr>
2021-03-23 09:01:17 +0100

    FOOD_EAT

---
+++ FOOD_EAT/main.js
@@ -0,0 +1,8 @@
+//Animation JS
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
+
+btn.addEventListener('click',() => {
+    overlay.style.display = 'grid';
+    overlay.classList.add('animate-overlay');
+});
